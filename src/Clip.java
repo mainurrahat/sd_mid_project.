e73056a276ed27e722351eba69021b39a113364e
Mainur Islam Rahat <114008697+mainurrahat@users.noreply.github.com>
2023-12-12 21:05:09 +0600

    Add files via upload

---
+++ src/Clip.java
@@ -0,0 +1,23 @@
+
+import javax.sound.sampled.AudioInputStream;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DELL
+ */
+class Clip {
+
+    void start() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void open(AudioInputStream audio) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
